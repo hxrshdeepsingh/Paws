594c03445b4ea0213f138de30d25d7aba124fd52
Harshdeep Singh <ishu.cxx@gmail.com>
2024-05-26 04:05:57 +0530

    monkey

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -3,7 +3,7 @@
 import Image from 'next/image'
 import Link from 'next/link';
 import { useState } from 'react';
-import { Squash as Hamburger } from 'hamburger-react';
+import { Slant as Hamburger } from 'hamburger-react'
 
 export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,7 +13,7 @@ export default function Header() {
     };
     return (
         <>
-            <nav className="bg-white border-gray-200 dark:bg-gray-900 text-xl">
+            <nav className="bg-white border-gray-200 dark:bg-gray-900 text-md">
                 <div className="container  flex flex-wrap items-center justify-between mx-auto py-4">
                     <Link href="/" className="flex items-center">
                         <Image src="/assets/logo.webp" width={100} height={50} alt="Flowbite Logo" />
@@ -24,15 +24,13 @@ export default function Header() {
                     </button>
 
                     <div className={`${isMenuOpen ? '' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-
+                        <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
                                 <Link className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' href="/posts/new">Create posts</Link>
                             </li>
                             <li>
                                 <Link className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' href="/posts/edit">Manage posts</Link>
                             </li>
-
                             <li>
                                 <Link href="/posts" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Explore</Link>
                             </li>
